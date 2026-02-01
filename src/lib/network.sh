bc68bb1cacb536432c3d81e7387c38ebc016eac0
Artem <artemkolba321@gmail.com>
2026-02-01 10:33:50 +0700

    Fix: minor changes

--- src/lib/network.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# OLS network helpers
-
-OLS_DIR="$HOME/OLS"
-STATE_FILE="$OLS_DIR/lib/network.state"
-
-net_online() {
-    [[ -f "$STATE_FILE" ]] && [[ "$(cat "$STATE_FILE")" == "online" ]]
-}
