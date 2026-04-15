5093a351ee4f6e8b039561feb698f246e30d26f0
Artem <artemkolba321@gmail.com>
2026-04-15 17:43:51 +0700

    Fix: minor changes

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ if [ "$AUTO_YES" = true ]; then
     confirm="y"
 else
     printf "This will modify your shell config. Continue? [y/N]: "
-    read confirm < /dev/tty
+    read -p confirm < /dev/tty
 fi
 if [[ "$confirm" != "y" && "$confirm" != "Y"  ]]; then
     
